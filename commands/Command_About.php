464026a7bee7fba4abaecda940b70450df310a90
Jackson Owens <jackson_owens@brown.edu>
2012-07-27 00:21:44 +0000

    Started integrating into esprit

---
+++ commands/Command_About.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace zc\commands;
+
+use \esprit\core\BaseCommand as BaseCommand;
+use \esprit\core\Request as Request;
+use \esprit\core\Response as Response;
+
+class Command_About extends BaseCommand {
+
+    const COMMAND_NAME = "About";
+
+    public function getName() {
+        return self::COMMAND_NAME;
+    }
+
+    public function run(Request $request, Response $response) {
+        return $response;
+    } 
+
+}
+
