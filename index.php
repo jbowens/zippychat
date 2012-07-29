cc306d0014462cfdc5ee8a6342652be59f2b23b8
Jackson Owens <jackson_owens@brown.edu>
2012-07-28 23:10:52 -0400

    Switched to extending templates

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-require_once "../web-lib/esprit/autoloader.php";
+require_once "../esprit/autoloader.php";
 require_once "autoloader.php";
 
 $config = esprit\core\Config::createFromJSON("data/config.json");
 $controller = esprit\core\Controller::createController( $config );
 
 // Setup logging
-$logRecorder = new esprit\core\util\FileLogRecorder("/var/www/logs/errors", esprit\core\util\Logger::WARNING);
+$logRecorder = new esprit\core\util\FileLogRecorder("/opt/local/apache2/htdocs/logs/errors", esprit\core\util\Logger::WARNING);
 $controller->getLogger()->addLogRecorder( $logRecorder );
 
 if( $config->get("debug") ) {
-    $fineRecorder = new esprit\core\util\FileLogRecorder("/var/www/logs/debug", esprit\core\util\Logger::FINEST);
+    $fineRecorder = new esprit\core\util\FileLogRecorder("/opt/local/apache2/htdocs/logs/debug", esprit\core\util\Logger::FINEST);
     $controller->getLogger()->addLogRecorder( $fineRecorder );
 }
 
