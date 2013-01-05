95166aa2a94182c3093a24b49ea940a01d5e2b22
Jackson Owens <jackson_owens@brown.edu>
2013-01-05 16:01:09 -0500

    Moved the other servers over to using AbstractAdServer

--- lib/adserver/GoogleAdSenseServer.php
+++ lib/adserver/GoogleAdSenseServer.php
@@ -17,65 +17,14 @@ use \zc\lib\AdType;
  * @author jbowens
  * @since 2012-08-26
  */
-class GoogleAdSenseServer implements AdServer
+class GoogleAdSenseServer extends AbstractAdServer
 {
-    use LogAware;
-    
-    protected $logger;
-    protected $availableAds = array();
 
-    public function __construct(Logger $logger)
+    protected function loadAvailableAds()
     {
-        $this->logger = $logger;
-
         array_push($this->availableAds, array(
                                     'adtype' => new \zc\lib\adtype\SkyscraperAd(),
                                     'advertisement' => new \zc\lib\ad\GoogleAdSenseSkyscraper() ) );
-    }
-
-    /**
-     * @see AdServer.canServe();
-     */
-    public function canServe( Request $request, AdType $adType )
-    {
-        foreach( $this->availableAds as $adTypeArr )
-        {
-            $availableAdType = $adTypeArr['adtype'];
-            $ad = $adTypeArr['advertisement'];
-            if( $availableAdType->withinType( $adType ) )
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * @see AdServer.getAd();
-     */
-    public function getAd( Request $request, AdType $adType )
-    {
-        // An easy way of enforcing the AdServer contract that if canServe()
-        // returns false, getAd() must return null.
-        if( ! $this->canServe( $request, $adType ) )
-        {
-            return null;
-        }
-
-        // Find the available ad
-        foreach( $this->availableAds as $adTypeArr )
-        {
-            $availableAdType = $adTypeArr['adtype'];
-            $ad = $adTypeArr['advertisement'];
-            if( $availableAdType->withinType( $adType ) )
-            {
-                return $ad;
-            }
-        }
-
-        // Impossible!
-        $this->error("Violation of the contract AdServer. getAd() returned null while canServe() returned true");
-        return null;
     }
 
 }
