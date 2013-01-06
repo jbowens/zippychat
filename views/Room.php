c9890e642966b8a67df11969c48bfbeeab1d4b67
Jackson Owens <jackson_owens@brown.edu>
2013-01-05 19:06:38 -0500

    Edited title text for chat rooms

--- views/Room.php
+++ views/Room.php
@@ -10,6 +10,7 @@ class Room extends DefaultView
     protected function output(Response $response)
     {
         $this->set('sharable_url', $this->urlUtil->generateRoomPermalink($response->get('room')));
+        $this->set('title_text', $response->get('room')->getTitle() . ' chat');
         $response->set('smallLogo', true);
         $response->set('widePage', true);
         $response->set('includeMetaTags', false);
