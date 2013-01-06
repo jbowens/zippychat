36fae19744e787662316bdd2316bc5ac2dace558
Jackson Owens <jackson_owens@brown.edu>
2013-01-05 19:21:12 -0500

    25% to Bidvertiser, 25% to Chitika, 50% nothing

--- commands/Command_Room.php
+++ commands/Command_Room.php
@@ -74,7 +74,7 @@ class Command_Room extends BaseCommand {
      */
     public function adsEnabled(Request $request)
     {
-        return ($this->config->get('serve_ads') == true);
+        return ($this->config->get('serve_ads') == true) && (rand(1,100) <= 50);
     }
 
     /**
@@ -92,7 +92,7 @@ class Command_Room extends BaseCommand {
         // We're currently displaying 120x600 skyscraper ads
         $adType = new SkyscraperAd();
 
-        $adSource = new ChitikaServer( $this->logger );
+        $adSource = (rand(1,100) <- 50) ? new ChitikaServer( $this->logger ) : new BidvertiserServer( $this->logger );
 
         if( ! $adSource->canServe( $request, $adType ) )
         {
