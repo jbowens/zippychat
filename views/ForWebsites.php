f26b984f3ca2c94b3224d8b1b7ca29eab1128214
Jackson Owens <jackson_owens@brown.edu>
2013-01-09 17:21:32 -0500

    Added masthead tags

---
+++ views/ForWebsites.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace zc\views;
+
+use \zc\lib\BaseView;
+
+use \esprit\core\Response;
+
+class ForWebsites extends BaseView {
+
+    const TEMPLATE = "ForWebsites";
+    const LOG_SOURCE = "views\\ForWebsites";
+
+    public function output( Response $response )
+    {
+        $this->templateParser->loadResponse( $response );
+
+        //$response->set('includeMetaTags', true);
+        $this->set('masthead_tag', ': for websites');
+
+        // Display the template
+        $this->templateParser->displayTemplate( self::TEMPLATE );
+    }
+
+}
